fc5e24242e35da5a247ad4a58c967771c68db61d
sarinha <sarah.mar.figueiredo@gmail.com>
2025-09-08 15:58:35 -0300

    d2_p4

--- desafio_barbara.py
+++ desafio_barbara.py
@@ -1,16 +1,22 @@
 import datetime #essa é uma library nativa do python, por isso apenas o import é suficiente para que ela funcione. No caso de librarys não nativas, precisamos instalar os arquivos na library no nosso computador usando o comando pip install 
 
+estoque = []
+
+def cadastrar_produto_estoque(nome, preco,qtd):
+    data = datetime.datetime.now() #precisamos criar uma variável local data, pois não recebemos esse valor nos parâmetros.
+    produto = (nome, preco, qtd, data)
+    print(f"Nome: {nome} \nPreço: {preco} \nQuantidade: {qtd} \nData: {data}")
+    estoque.append(produto)
+    print("Produto Cadastrado.")  
+
 resposta = input("Deseja cadastrar um novo produto? (S/N) ")
 while resposta.upper() == "S": 
     nome = input("Digite o nome do produto: ")
     preco = float(input("Digite o preço do produto: "))
     qtd = int(input("Digite a quantidade do produto em estoque: "))
+    cadastrar_produto_estoque(nome,preco,qtd) #para que a função cadastrar_produto_estoque funcione corretamente, ela deve ser chamada a cada entrada de dados de um novo produto
     resposta = input("Deseja cadastrar um novo produto? (S/N) ") 
 
-def cadastrar_produto_estoque(nome, preco,qtd): #aqui, "nome", "preco" e "qtd" são parâmetros, ou seja, podem receber qualquer valor quando a função é chamada e não são necessariamente as variáveis do laço while acima
-    print(f"Nome: {nome} \nPreço: {preco} \nQuantidade: {qtd} \nData: {datetime.datetime.now()}")
-    #/n adiciona parágrafos ao nosso print
-    #a função datetime.datetime.now() é uma função da biblioteca adicionada, que mostra  a data e hora exatos do momento em que é chamada
-    print("Produto Cadastrado.")  
 
-cadastrar_produto_estoque(nome,preco,qtd)
+
+
