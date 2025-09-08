5721a2b1136730dee37fc0cda526bed4faa6ce5e
sarinha <sarah.mar.figueiredo@gmail.com>
2025-09-08 14:05:28 -0300

    dia1_parte2

--- desafio_barbara.py
+++ desafio_barbara.py
@@ -1,3 +1,6 @@
+import datetime
+#essa é uma library nativa do python, por isso apenas o import é suficiente para que ela funcione. No caso de librarys não nativas, precisamos instalar os arquivos na library no nosso computador usando o comando pip install 
+
 produtos = []
 
 resposta = input("Deseja cadastrar um novo produto? (S/N) ")
@@ -7,11 +10,12 @@ while resposta.upper() == "S":
     preco = float(input("Digite o preço do produto: "))
     qtd = int(input("Digite a quantidade do produto em estoque: "))
     produto = [nome, preco, qtd]
+    print(f"Nome: {produto[0]} \nPreço: {produto[1]} \nQuantidade: {produto[2]} \nData: {datetime.datetime.now()}")
     produtos.append(produto)
     resposta = input("Deseja cadastrar um novo produto? (S/N)")
 
 print("-------------------")
-print("Cadastro encerrado")
+print("Cadastro encerrado.")
 
 #utilizamos a variável de controle resposta para que o processo de cadastro de produtos seja continuamente controlado pela usuária.
 #poderíamos adicionar, nesse código, outras validações, como uma condicional para o caso de a resposta ser diferente de S ou N, a entrada em qtd ser diferente de um número inteiro
