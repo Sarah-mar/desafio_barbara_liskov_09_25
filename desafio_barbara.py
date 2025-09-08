e39dbba4818d7b30b271a77de1a4c81af6df2db0
sarinha <sarah.mar.figueiredo@gmail.com>
2025-09-08 14:41:06 -0300

    d1_p1

--- desafio_barbara.py
+++ desafio_barbara.py
@@ -1,17 +1,8 @@
-import datetime
-#essa é uma library nativa do python, por isso apenas o import é suficiente para que ela funcione. No caso de librarys não nativas, precisamos instalar os arquivos na library no nosso computador usando o comando pip install 
-
-produtos = []
-
 resposta = input("Deseja cadastrar um novo produto? (S/N) ")
-while resposta.upper() == "S":
-    #a função .upper() transforma a variável resposta em letras maiúsculas, caso a entrada tenha alguma letra minúscula
+while resposta == "S":
     nome = input("Digite o nome do produto: ")
     preco = float(input("Digite o preço do produto: "))
     qtd = int(input("Digite a quantidade do produto em estoque: "))
-    produto = [nome, preco, qtd]
-    print(f"Nome: {produto[0]} \nPreço: {produto[1]} \nQuantidade: {produto[2]} \nData: {datetime.datetime.now()}")
-    produtos.append(produto)
     resposta = input("Deseja cadastrar um novo produto? (S/N)")
 
 print("-------------------")
