b5ee20dfe0c83e6c02798c1ed45382b0bb236155
sarinha <sarah.mar.figueiredo@gmail.com>
2025-09-08 15:09:09 -0300

    d1_p2

--- desafio_barbara.py
+++ desafio_barbara.py
@@ -1,17 +1,13 @@
-import datetime #essa é uma library nativa do python, por isso apenas o import é suficiente para que ela funcione. No caso de librarys não nativas, precisamos instalar os arquivos na library no nosso computador usando o comando pip install 
 
 resposta = input("Deseja cadastrar um novo produto? (S/N) ")
 while resposta.upper() == "S": #a função .upper() transforma a variável resposta em letras maiúsculas, caso a entrada tenha alguma letra minúscula
     nome = input("Digite o nome do produto: ")
     preco = float(input("Digite o preço do produto: "))
     qtd = int(input("Digite a quantidade do produto em estoque: "))
-    print(f"Nome: {nome} \nPreço: {preco} \nQuantidade: {qtd} \nData: {datetime.datetime.now()}")
-    #/n adiciona parágrafos ao nosso print
-    #a função datetime.datetime.now() é uma função da biblioteca adicionada, que mostra  a data e hora exatos do momento em que é chamada
-    resposta = input("Deseja cadastrar um novo produto? (S/N)") #atualização da resposta da usuária, que definirá se o laço while continuará rodando ou não.
+    resposta = input("Deseja cadastrar um novo produto? (S/N)")
 
-print("-------------------")
-print("Cadastro encerrado.")
+def cadastrar_produto_estoque(nome, preco,qtd):
+    print("Produto Cadastrado.")  
 
 #utilizamos a variável de controle resposta para que o processo de cadastro de produtos seja continuamente controlado pela usuária.
 #poderíamos adicionar, nesse código, outras validações, como uma condicional para o caso de a resposta ser diferente de S ou N, a entrada em qtd ser diferente de um número inteiro
