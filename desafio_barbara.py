a0ef6a3bb703a41058bf0925b47c59bda3f9175d
sarinha <sarah.mar.figueiredo@gmail.com>
2025-09-08 16:12:05 -0300

    d2_p5

--- desafio_barbara.py
+++ desafio_barbara.py
@@ -1,22 +1,29 @@
-import datetime #essa é uma library nativa do python, por isso apenas o import é suficiente para que ela funcione. No caso de librarys não nativas, precisamos instalar os arquivos na library no nosso computador usando o comando pip install 
+import datetime 
 
 estoque = []
 
 def cadastrar_produto_estoque(nome, preco,qtd):
-    data = datetime.datetime.now() #precisamos criar uma variável local data, pois não recebemos esse valor nos parâmetros.
+    data = datetime.datetime.now() 
     produto = (nome, preco, qtd, data)
     print(f"Nome: {nome} \nPreço: {preco} \nQuantidade: {qtd} \nData: {data}")
     estoque.append(produto)
     print("Produto Cadastrado.")  
+
+def exibir_estoque():
+    if len(estoque) != 0:
+        print("Estoque:")
+        for produto in estoque: #Estamos percorrendo o estoque elemento por elemento (nesse caso, os elementos são os objetos produto, que tem o tipo lista)
+            print("produto:")
+            print(f"Nome: {produto[0]} \nPreço: {produto[1]} \nQuantidade: {produto[2]} \nData: {produto[3]}")
+    else:
+        print ("Estoque vazio")
 
 resposta = input("Deseja cadastrar um novo produto? (S/N) ")
 while resposta.upper() == "S": 
     nome = input("Digite o nome do produto: ")
     preco = float(input("Digite o preço do produto: "))
     qtd = int(input("Digite a quantidade do produto em estoque: "))
-    cadastrar_produto_estoque(nome,preco,qtd) #para que a função cadastrar_produto_estoque funcione corretamente, ela deve ser chamada a cada entrada de dados de um novo produto
+    cadastrar_produto_estoque(nome,preco,qtd) 
     resposta = input("Deseja cadastrar um novo produto? (S/N) ") 
-
-
 
 
