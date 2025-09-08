939634703179d1f96cde0f3435a48b0c716d13ec
sarinha <sarah.mar.figueiredo@gmail.com>
2025-09-08 14:33:34 -0300

    dia 1, parte 2

--- desafio_barbara.py
+++ desafio_barbara.py
@@ -1,18 +1,14 @@
-import datetime
-#essa é uma library nativa do python, por isso apenas o import é suficiente para que ela funcione. No caso de librarys não nativas, precisamos instalar os arquivos na library no nosso computador usando o comando pip install 
-
-produtos = []
+import datetime #essa é uma library nativa do python, por isso apenas o import é suficiente para que ela funcione. No caso de librarys não nativas, precisamos instalar os arquivos na library no nosso computador usando o comando pip install 
 
 resposta = input("Deseja cadastrar um novo produto? (S/N) ")
-while resposta.upper() == "S":
-    #a função .upper() transforma a variável resposta em letras maiúsculas, caso a entrada tenha alguma letra minúscula
+while resposta.upper() == "S": #a função .upper() transforma a variável resposta em letras maiúsculas, caso a entrada tenha alguma letra minúscula
     nome = input("Digite o nome do produto: ")
     preco = float(input("Digite o preço do produto: "))
     qtd = int(input("Digite a quantidade do produto em estoque: "))
-    produto = [nome, preco, qtd]
-    print(f"Nome: {produto[0]} \nPreço: {produto[1]} \nQuantidade: {produto[2]} \nData: {datetime.datetime.now()}")
-    produtos.append(produto)
-    resposta = input("Deseja cadastrar um novo produto? (S/N)")
+    print(f"Nome: {nome} \nPreço: {preco} \nQuantidade: {qtd} \nData: {datetime.datetime.now()}")
+    #/n adiciona parágrafos ao nosso print
+    #a função datetime.datetime.now() é uma função da biblioteca adicionada, que mostra  a data e hora exatos do momento em que é chamada
+    resposta = input("Deseja cadastrar um novo produto? (S/N)") #atualização da resposta da usuária, que definirá se o laço while continuará rodando ou não.
 
 print("-------------------")
 print("Cadastro encerrado.")
